9bda82f0394c570ad3263a22320e6de1bd4f347c
sdarwin <samuel.d.darwin@gmail.com>
2020-12-23 14:57:14 +0000

    drone

---
+++ .drone/boost.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -ex
+export TRAVIS_BUILD_DIR=$(pwd)
+export TRAVIS_BRANCH=$DRONE_BRANCH
+export TRAVIS_OS_NAME=${DRONE_JOB_OS_NAME:-linux}
+export VCS_COMMIT_ID=$DRONE_COMMIT
+export GIT_COMMIT=$DRONE_COMMIT
+export DRONE_CURRENT_BUILD_DIR=$(pwd)
+export PATH=~/.local/bin:/usr/local/bin:$PATH
+
+echo '==================================> BEFORE_INSTALL'
+
+. .drone/before-install.sh
+
+echo '==================================> INSTALL'
+
+cd ..
+git clone -b $TRAVIS_BRANCH --depth 1 https://github.com/boostorg/boost.git boost-root
+cd boost-root
+git submodule update --init tools/build
+git submodule update --init libs/config
+git submodule update --init tools/boostdep
+git submodule update --init tools/boost_install
+git submodule update --init libs/headers
+cp -r $TRAVIS_BUILD_DIR/* libs/tti
+python tools/boostdep/depinst/depinst.py tti
+./bootstrap.sh
+./b2 headers
+
+echo '==================================> BEFORE_SCRIPT'
+
+. $DRONE_CURRENT_BUILD_DIR/.drone/before-script.sh
+
+echo '==================================> SCRIPT'
+
+TOOLSET=gcc,clang
+if [ $TRAVIS_OS_NAME == osx ]; then TOOLSET=clang; fi
+./b2 --verbose-test libs/config/test//config_info toolset=$TOOLSET || true
+./b2 libs/tti/test toolset=$TOOLSET
+
+echo '==================================> AFTER_SUCCESS'
+
+. $DRONE_CURRENT_BUILD_DIR/.drone/after-success.sh
